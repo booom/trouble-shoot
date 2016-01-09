43036f25590536c88b915605de8d93ad24953785
jason <jason@wemart.cn>
2016-01-09 16:21:12 +0800

    add day-dbbackup-cleanexpiredback.sh

---
+++ shell-scripts/day-dbbackup-cleanexpiredback.sh
@@ -0,0 +1,13 @@
+#crontab
+#30 3 * * * /data/opscripts/day-dbbackup-cleanexpiredback.sh dbname
+
+backupdir=/data/dbbackup
+dbname=$1
+
+reservedays=30
+
+time=` date +%Y%m%d%H `
+
+mysqldump --default-character-set=utf8mb4 --single-transaction --set-gtid-purged=OFF -q -R -uuser -ppasswd -hlocalhost $dbname | gzip > $backupdir/$dbname$time.sql.gz
+
+find $backupdir -name "$dbname*.sql.gz" -type f -mtime +$reservedays -exec rm {} \; > /dev/null 2>&1
